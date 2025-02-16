25c9d243337b9e642a5cbdeb1c13cc17b30ebf59
EmmaKDH <99865228+EmmaKDH@users.noreply.github.com>
2025-02-16 14:44:34 +0100

    Add files via upload

---
+++ Code and Development Iterations/Base Watch Functionality/Squareline Files/SmartWatch_Project_V1_events.py
@@ -0,0 +1,44 @@
+
+def ResetSecond(event_struct):
+    return
+
+
+def TimeSetMinuteDown(event_struct):
+    return
+
+
+def TimeSetMinuteUp(event_struct):
+    return
+
+
+def TimeSetHourUp(event_struct):
+    return
+
+
+def TimeSetHourDown(event_struct):
+    return
+
+
+def DisplaySleepCheck(event_struct):
+    return
+
+
+def OnHomeScreen(event_struct):
+    return
+
+
+def OffHomeScreen(event_struct):
+    return
+
+
+def DisplaySleepmode(event_struct):
+    return
+
+
+def DisplayWakeup(event_struct):
+    return
+
+
+def DisplaySleep(event_struct):
+    return
+
